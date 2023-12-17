fc6729856930b7d2f7595d4fa19b275c1ed0fcec
arafatGithubCode <md489288@gmail.com>
2023-12-17 08:38:24 +0600

    Add data to firestore

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,87 @@ import { useEffect, useState } from "react";
 import Auth from "./components/Auth";
 
 import { db } from "./config/firebase";
-import { getDocs, collection } from "firebase/firestore";
+import { getDocs, collection, addDoc } from "firebase/firestore";
 
 export default function App() {
   const [movieList, setMovieList] = useState([]);
 
+  const [movieTitle, setMovieTitle] = useState("");
+  const [releaseDate, setReleaseDate] = useState(0);
+  const [isWatch, setIsWatch] = useState(true);
+
   const movieCollectionRef = collection(db, "movies");
 
+  const getMovieList = async () => {
+    try {
+      const data = await getDocs(movieCollectionRef);
+      const filterData = data.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
+      setMovieList(filterData);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   useEffect(() => {
-    const getMovieList = async () => {
-      try {
-        const data = await getDocs(movieCollectionRef);
-        const filterData = data.docs.map((doc) => ({
-          ...doc.data(),
-          id: doc.id,
-        }));
-        setMovieList(filterData);
-      } catch (error) {
-        console.error(error);
-      }
-    };
     getMovieList();
   }, []);
+
+  const submitMovie = async () => {
+    try {
+      await addDoc(movieCollectionRef, {
+        title: movieTitle,
+        releaseDate: releaseDate,
+        watched: isWatch,
+      });
+      getMovieList();
+      setMovieTitle("");
+      setIsWatch(false);
+      setReleaseDate(0);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <>
       <h1>firebase finishing</h1>
       <Auth />
+      <div>
+        <label htmlFor="movieName">Title:</label>
+        <input
+          type="text"
+          name="movieName"
+          id="movieName"
+          placeholder="movie name.."
+          value={movieTitle}
+          onChange={(e) => setMovieTitle(e.target.value)}
+        />
+        <br />
+        <label htmlFor="date">Release Date:</label>
+        <input
+          type="number"
+          name="date"
+          id="date"
+          placeholder="Release date"
+          value={releaseDate}
+          onChange={(e) => setReleaseDate(Number(e.target.value))}
+        />
+        <br />
+        <input
+          type="checkbox"
+          name="watch"
+          id="watch"
+          checked={isWatch}
+          onChange={(e) => setIsWatch(e.target.checked)}
+        />
+        <label htmlFor="watch">Watch:</label>
+        <br />
+        <button onClick={submitMovie}>submit movie</button>
+      </div>
+
       {movieList.map((movie, index) => (
         <article key={index}>
           <h3 style={{ color: movie.watched ? "green" : "red" }}>
